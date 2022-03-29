81232da4aba7460a58c9d735c47a97a14e11a10b
Jackson <gudo1013@stthomas.edu>
2022-03-29 14:25:29 -0500

    testing commit

--- scripts/matrix.js
+++ scripts/matrix.js
@@ -15,7 +15,7 @@ class Matrix {
 
     set values(v) {
         var i, j, idx;
-        // v is already a 2d array with dims equal to rows and columns
+        // v is already a 2d array with dims equal to rows and columns //
         if (v instanceof Array && v.length === this.rows && 
             v[0] instanceof Array && v[0].length === this.columns) {
             this.data = v;
